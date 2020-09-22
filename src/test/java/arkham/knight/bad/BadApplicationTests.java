736d3c9e052dbf11150c8f85bb13e36813b59189
kevin <karvin_jimenez@hotmail.com>
2020-09-21 21:15:26 -0400

    Dependencia spring boot dev tool agregada

--- src/test/java/arkham/knight/bad/BadApplicationTests.java
+++ src/test/java/arkham/knight/bad/BadApplicationTests.java
@@ -1,13 +1,26 @@
 package arkham.knight.bad;
 
+import arkham.knight.bad.models.Cast;
+import arkham.knight.bad.services.CastService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class BadApplicationTests {
 
+    @Autowired
+    CastService castService;
+
+    Cast cast = new Cast();
+
     @Test
-    void contextLoads() {
+    void testFindCastByName() {
+
+        cast.setName("Walter White");
+
+        assertEquals(cast.getName(), castService.findCastByName("Walter White").getName());
     }
 
 }
